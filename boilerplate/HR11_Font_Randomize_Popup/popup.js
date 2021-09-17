157bb11e5f03e3a0868b223a33546d4c9571ea45
Rithik <Jain>
2021-09-17 11:12:59 -0500

    minor fix

--- boilerplate/HR11_Font_Randomize_Popup/popup.js
+++ boilerplate/HR11_Font_Randomize_Popup/popup.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
     var changeFontButton = document.getElementById('changeFont');
     changeFontButton.addEventListener('click', function(){
-        chrome.tabs.getSelected(null, (tab) => {
-            chrome.tabs.executeScript(tab.id, {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.executeScript(activeTab.id, {
                 file: 'inject.js'
             });
         });
